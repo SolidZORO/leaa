676a298b3c796cfa12cf074ccf1651960e47bc7f
SolidZORO <solidzoro@live.com>
2019-06-11 02:02:29 +0800

    add Base Service C U R D findOne, write a pretty baseCURD is too hard T_T

--- packages/_leaa-common/src/dtos/user/user.object.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectType, Field } from 'type-graphql';
-
-import { User } from '@leaa/common/entrys';
-
-@ObjectType()
-export class UserResponse {
-  @Field(() => User)
-  readonly item?: User;
-}
