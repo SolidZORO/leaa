c5038242bb0d1dc13b58838b94fefe5fc582ac53
SolidZORO <solidzoro@live.com>
2019-07-06 01:40:20 +0800

    update user edit add refetchQueries

--- packages/leaa-dashboard/src/pages/User/UserEdit/UserEdit.tsx
+++ packages/leaa-dashboard/src/pages/User/UserEdit/UserEdit.tsx
@@ -22,46 +22,43 @@ export default (props: IPage) => {
   let userInfoFormRef: any; // eslint-disable-line @typescript-eslint/no-explicit-any
   let userRoleFormRef: any; // eslint-disable-line @typescript-eslint/no-explicit-any
 
-  const variables = { id: Number(id) };
-  const { loading, data: userData, error: userError } = useQuery<{ user: User }, UserArgs>(GET_USER, { variables });
+  const getUserVariables = { id: Number(id) };
+  const { loading, data: userData, error: userError } = useQuery<{ user: User }, UserArgs>(GET_USER, {
+    variables: getUserVariables,
+  });
 
   if (userError) {
     return <ErrorCard message={userError.message} />;
   }
 
+  const getRolesVariables = { pageSize: 9999 };
   const { data: rolesData, error: rolesError } = useQuery<{ roles: RolesObject }, RolesArgs>(GET_ROLES, {
-    variables: {
-      pageSize: 9999,
-    },
+    variables: getRolesVariables,
   });
 
   if (rolesError) {
     return <ErrorCard message={rolesError.message} />;
   }
 
-  const [submitVariables, setSubmitVariables] = useState<UpdateUserInput | undefined>();
+  const [submitVariables, setSubmitVariables] = useState<{ id: number; user: UpdateUserInput }>({
+    id: Number(id),
+    user: {},
+  });
 
   const [updateUser, { loading: submitLoading }] = useMutation<User>(UPDATE_USER, {
-    variables: {
-      id: Number(id),
-      user: submitVariables,
-    },
+    variables: submitVariables,
     onError(e) {
       message.error(e.message);
     },
-    refetchQueries: ['GET_USER'],
-    onCompleted(user) {
+    onCompleted() {
       message.success('Update Successful');
     },
+    refetchQueries: () => [{ query: GET_USER, variables: getUserVariables }],
   });
 
   const onSubmit = async () => {
     let hasError = false;
-    let submitData: UpdateUserInput = {
-      roleIds: [],
-    };
-
-    // setUiSubmitLoading(true);
+    let submitData: UpdateUserInput = { roleIds: [] };
 
     userRoleFormRef.props.form.validateFieldsAndScroll(async (err: Error, formData: { roleIds: number[] }) => {
       if (err) {
@@ -88,8 +85,10 @@ export default (props: IPage) => {
       };
     });
 
-    console.log(submitData.status);
-    await setSubmitVariables(submitData);
+    await setSubmitVariables({
+      ...submitVariables,
+      ...{ user: submitData },
+    });
     await updateUser();
   };
 
