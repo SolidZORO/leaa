64a2b3c85eb9ea1d81158024ab1d312023daef8e
SolidZORO <solidzoro@live.com>
2019-07-22 18:24:35 +0800

    fixe link href

--- packages/leaa-www/pages/index/_components/BannerSwiper/BannerSwiper.tsx
+++ packages/leaa-www/pages/index/_components/BannerSwiper/BannerSwiper.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import getConfig from 'next/config';
 import Swiper from 'react-id-swiper';
 import { useQuery } from '@apollo/react-hooks';
+import Link from 'next/link';
 
 import { GET_AX_BY_SLUG } from '@leaa/common/graphqls';
 import { Ax } from '@leaa/common/entrys';
@@ -9,7 +10,6 @@ import { AxArgs } from '@leaa/common/dtos/ax';
 import { ErrorCard } from '@leaa/www/components/ErrorCard/ErrorCard';
 
 import style from './style.less';
-import Link from 'next/link';
 
 export const BannerSwiper = () => {
   const { publicRuntimeConfig } = getConfig();
@@ -46,11 +46,13 @@ export const BannerSwiper = () => {
         }}
       >
         {bannerMbList.map(a => (
-          <Link href={a.link}>
-            <a className={style['link']}>
-              <img key={a.uuid} src={`${publicRuntimeConfig.API_HOST}${a.path}`} alt="" />
-            </a>
-          </Link>
+          <div key={a.uuid}>
+            <Link href={`/${a.link}`}>
+              <a>
+                <img src={`${publicRuntimeConfig.API_HOST}${a.path}`} alt="" />
+              </a>
+            </Link>
+          </div>
         ))}
       </Swiper>
     </div>
