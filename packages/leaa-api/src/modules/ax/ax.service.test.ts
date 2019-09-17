0c38192695310ce47d9afc1d1b15bca1feac0776
SolidZORO <solidzoro@live.com>
2019-09-17 17:34:50 +0800

    add some test

---
+++ packages/leaa-api/src/modules/ax/ax.service.test.ts
@@ -0,0 +1,116 @@
+import { Repository } from 'typeorm';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { Ax, Category } from '@leaa/common/src/entrys';
+import { CreateAxInput, UpdateAxInput } from '@leaa/common/src/dtos/ax';
+import { AxService } from '@leaa/api/src/modules/ax/ax.service';
+
+describe('AxService', () => {
+  let axService: AxService;
+  const ARTICLE_REPOSITORY_MOCK: Repository<Ax> = new Repository<Ax>();
+  const CATEGORY_REPOSITORY_MOCK: Repository<Category> = new Repository<Category>();
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AxService,
+        {
+          provide: getRepositoryToken(Ax),
+          useValue: ARTICLE_REPOSITORY_MOCK,
+        },
+        {
+          provide: getRepositoryToken(Category),
+          useValue: CATEGORY_REPOSITORY_MOCK,
+        },
+      ],
+    }).compile();
+
+    axService = module.get<AxService>(AxService);
+  });
+
+  it('should be server defined', () => {
+    expect(axService).toBeDefined();
+  });
+
+  let axObject = new Ax();
+
+  describe('craeteAx', () => {
+    const createAxInput: CreateAxInput = {
+      title: 'title',
+      slug: 'slug',
+      description: 'description',
+      status: 1,
+    };
+
+    axObject = {
+      ...createAxInput,
+      id: 1,
+      created_at: new Date(),
+    };
+
+    it('should create ax', async () => {
+      jest.spyOn(axService, 'craeteAx').mockImplementation(async () => axObject);
+      const result = await axService.craeteAx(createAxInput);
+
+      expect(result).toBe(axObject);
+    });
+  });
+
+  describe('updateAx', () => {
+    const updateAxInput: UpdateAxInput = { title: 'update' };
+
+    const updatedAx: Ax = {
+      ...axObject,
+      ...updateAxInput,
+    };
+
+    it('should update ax', async () => {
+      jest.spyOn(axService, 'updateAx').mockImplementation(async () => updatedAx);
+      const result = await axService.updateAx(1, updateAxInput);
+
+      expect(result).toBe(updatedAx);
+    });
+  });
+
+  describe('getAx', () => {
+    const axsObject = {
+      items: [axObject, axObject],
+      page: 1,
+      pageSize: 30,
+      total: 10,
+    };
+
+    it('should return axs', async () => {
+      jest.spyOn(axService, 'axs').mockImplementation(async () => axsObject);
+      const result = await axService.axs({});
+
+      expect(result).toBe(axsObject);
+    });
+
+    it('should return ax', async () => {
+      jest.spyOn(axService, 'ax').mockImplementation(async () => axObject);
+      const result = await axService.ax(1);
+
+      expect(result).toBe(axObject);
+    });
+
+    it('should return ax (undefined)', async () => {
+      jest.spyOn(axService, 'ax').mockImplementation(async () => undefined);
+      const result = await axService.ax(2);
+
+      expect(result).toBe(undefined);
+    });
+  });
+
+  describe('deleteAx', () => {
+    const deletedAx = undefined;
+
+    it('should delete ax', async () => {
+      jest.spyOn(axService, 'deleteAx').mockImplementation(async () => deletedAx);
+      const result = await axService.deleteAx(1);
+
+      expect(result).toBe(deletedAx);
+    });
+  });
+});
---
+++ packages/leaa-api/src/modules/category/category.service.test.ts
@@ -0,0 +1,116 @@
+import { Repository } from 'typeorm';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { Category } from '@leaa/common/src/entrys';
+import { CreateCategoryInput, UpdateCategoryInput } from '@leaa/common/src/dtos/category';
+import { CategoryService } from '@leaa/api/src/modules/category/category.service';
+
+describe('CategoryService', () => {
+  let categoryService: CategoryService;
+  const ARTICLE_REPOSITORY_MOCK: Repository<Category> = new Repository<Category>();
+  const CATEGORY_REPOSITORY_MOCK: Repository<Category> = new Repository<Category>();
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CategoryService,
+        {
+          provide: getRepositoryToken(Category),
+          useValue: ARTICLE_REPOSITORY_MOCK,
+        },
+        {
+          provide: getRepositoryToken(Category),
+          useValue: CATEGORY_REPOSITORY_MOCK,
+        },
+      ],
+    }).compile();
+
+    categoryService = module.get<CategoryService>(CategoryService);
+  });
+
+  it('should be server defined', () => {
+    expect(categoryService).toBeDefined();
+  });
+
+  let categoryObject = new Category();
+
+  describe('craeteCategory', () => {
+    const createCategoryInput: CreateCategoryInput = {
+      name: 'name',
+      slug: 'slug',
+      description: 'description',
+    };
+
+    categoryObject = {
+      ...createCategoryInput,
+      id: 1,
+      parent_id: 0,
+      created_at: new Date(),
+    };
+
+    it('should create category', async () => {
+      jest.spyOn(categoryService, 'craeteCategory').mockImplementation(async () => categoryObject);
+      const result = await categoryService.craeteCategory(createCategoryInput);
+
+      expect(result).toBe(categoryObject);
+    });
+  });
+
+  describe('updateCategory', () => {
+    const updateCategoryInput: UpdateCategoryInput = { name: 'update' };
+
+    const updatedCategory: Category = {
+      ...categoryObject,
+      ...updateCategoryInput,
+    };
+
+    it('should update category', async () => {
+      jest.spyOn(categoryService, 'updateCategory').mockImplementation(async () => updatedCategory);
+      const result = await categoryService.updateCategory(1, updateCategoryInput);
+
+      expect(result).toBe(updatedCategory);
+    });
+  });
+
+  describe('getCategory', () => {
+    const categoriesObject = {
+      items: [categoryObject, categoryObject],
+      page: 1,
+      pageSize: 30,
+      total: 10,
+    };
+
+    it('should return categories', async () => {
+      jest.spyOn(categoryService, 'categories').mockImplementation(async () => categoriesObject);
+      const result = await categoryService.categories({});
+
+      expect(result).toBe(categoriesObject);
+    });
+
+    it('should return category', async () => {
+      jest.spyOn(categoryService, 'category').mockImplementation(async () => categoryObject);
+      const result = await categoryService.category(1);
+
+      expect(result).toBe(categoryObject);
+    });
+
+    it('should return category (undefined)', async () => {
+      jest.spyOn(categoryService, 'category').mockImplementation(async () => undefined);
+      const result = await categoryService.category(2);
+
+      expect(result).toBe(undefined);
+    });
+  });
+
+  describe('deleteCategory', () => {
+    const deletedCategory = undefined;
+
+    it('should delete category', async () => {
+      jest.spyOn(categoryService, 'deleteCategory').mockImplementation(async () => deletedCategory);
+      const result = await categoryService.deleteCategory(1);
+
+      expect(result).toBe(deletedCategory);
+    });
+  });
+});
