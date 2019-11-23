46499940d7126b72f46ecf2553a6afa03f0b5618
SolidZORO <solidzoro@live.com>
2019-11-23 12:51:36 +0800

    update force refresh style.chunk.css ONLY for dev mode

--- packages/leaa-www/src/pages/_app.tsx
+++ packages/leaa-www/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React, { ErrorInfo } from 'react';
 import App from 'next/app';
 import { ApolloProvider } from '@apollo/react-hooks';
+import Router from 'next/router';
 
 import { IPageProps } from '@leaa/www/src/interfaces';
 import { withApolloClient } from '@leaa/www/src/libs/with-apollo-client.lib';
@@ -9,6 +10,20 @@ import { ProgressLoading } from '@leaa/www/src/components/ProgressLoading';
 import { initStore, StoreProvider } from '@leaa/www/src/stores';
 
 const store = initStore();
+
+// force refresh style.chunk.css ONLY for dev mode
+if (process.env.NODE_ENV !== 'production') {
+  Router.events.on('routeChangeComplete', () => {
+    const path = '/_next/static/css/styles.chunk.css';
+    const chunksNodes = document.querySelectorAll(`link[href*="${path}"]`);
+
+    if (chunksNodes.length) {
+      // @ts-ignore
+      // eslint-disable-next-line no-return-assign,no-param-reassign
+      chunksNodes.forEach(chunk => (chunk.href = `${path}?force-refresh-ts=${new Date().getTime()}`));
+    }
+  });
+}
 
 class CustomApp extends App<IPageProps> {
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
