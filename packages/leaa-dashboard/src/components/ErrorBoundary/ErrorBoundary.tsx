6afe5684a9376d0afcd030adb1225a51487b1079
SolidZORO <solidzoro@live.com>
2019-11-28 01:32:50 +0800

    update optimize componentDidCatch error tips

--- packages/leaa-dashboard/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ packages/leaa-dashboard/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import queryString from 'query-string';
 import { Button } from 'antd';
+
+import { urlUtil } from '@leaa/dashboard/src/utils';
 
 import style from './style.module.less';
 
@@ -9,6 +12,8 @@ interface IState {
   hasError: boolean;
   errorInfo: {};
 }
+
+const CATCH_HAS_REFRESH_URL_PARAM = '____catch-has-refresh';
 
 export class ErrorBoundary extends React.Component<IProps, IState> {
   constructor(props: IProps) {
@@ -24,7 +29,21 @@ export class ErrorBoundary extends React.Component<IProps, IState> {
   }
 
   componentDidCatch(error: Error, info: {}) {
-    this.setState({ errorInfo: info });
+    // TIPS: Many times DidCatch is because the JS file can't be retrieved, so refresh it first.
+    const qs = queryString.parse(window.location.search);
+
+    if (!qs[CATCH_HAS_REFRESH_URL_PARAM]) {
+      window.location.href = urlUtil.mergeParamToUrlQuery({
+        window,
+        params: { [CATCH_HAS_REFRESH_URL_PARAM]: 1 },
+        replace: false,
+      });
+    }
+
+    console.log('ALL-STACK:', info);
+    console.log('ALL-ERROR:', error);
+
+    this.setState({ errorInfo: error.message });
   }
 
   onGoToHome = () => {
