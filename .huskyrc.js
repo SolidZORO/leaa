2de56b1d66d31b6a9bad0aca6627df7a6df17a23
SolidZORO <solidzoro@live.com>
2020-03-23 14:55:54 +0800

    update auto pretty and lint-staged

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,17 @@
+const tasks = (arr) => arr.join(' && ');
+
 module.exports = {
   hooks: {
-    'pre-commit': 'lint-staged',
+    'pre-commit': tasks([
+      //check nx before committing!!
+      // uncommented for now
+      //'nx test',
+      // pretty quick is explained here
+      // https://github.com/azz/pretty-quick
+      'pretty-quick --staged',
+      // lint-staged is explained here
+      // https://github.com/okonet/lint-staged#configuration.
+      'lint-staged',
+    ]),
   },
 };
