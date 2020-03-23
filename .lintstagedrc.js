7ccfaff3798b277c814d19a239aa6af258206bb7
SolidZORO <solidzoro@live.com>
2020-03-23 15:17:56 +0800

    update lint-staged for js|ts

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'packages/**/*.ts?(x)': ['prettier --write', 'eslint'],
+  'packages/**/*.(ts|js)?(x)': ['prettier --write', 'eslint'],
   // 'packages/**/*.(css|less)': ['prettier --write', 'stylelint"', 'git add'],
 };
