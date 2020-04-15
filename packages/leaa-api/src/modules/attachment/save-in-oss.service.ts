4bdbda893c1df7345feebbdadb19b8b3905409c9
SolidZORO <solidzoro@live.com>
2020-04-16 02:42:15 +0800

    update .lintstagedrc rule

--- packages/leaa-api/src/modules/attachment/save-in-oss.service.ts
+++ packages/leaa-api/src/modules/attachment/save-in-oss.service.ts
@@ -135,8 +135,6 @@ export class SaveInOssService {
 
   async saveOssToLocal(
     attachment: Pick<Attachment, 'filename' | 'url' | 'urlAt2x' | 'at2x'>,
-
-
   ): Promise<'success' | Error> {
     await this.downloadFile(attachment.url || '', (file) => {
       try {
