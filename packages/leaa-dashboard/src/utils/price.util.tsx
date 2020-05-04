bea94f264de5ae0c4ee729db5243bc3fd3b78e1a
SolidZORO <solidzoro@live.com>
2020-05-04 14:48:10 +0800

    refactor: clear package

--- packages/leaa-dashboard/src/utils/price.util.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Dinero, { Currency } from 'dinero.js';
-
-const formatPrice = (amount: number, currency: Currency = 'CNY') => Dinero({ amount, currency });
-
-export const priceUtil = {
-  formatPrice,
-};
