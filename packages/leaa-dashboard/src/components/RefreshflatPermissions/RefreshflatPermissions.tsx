1c1bfb0f2cf9d730e5b4c6ca9cc846bc797024e5
SolidZORO <solidzoro@live.com>
2020-05-22 16:06:15 +0800

    refactor: test huskyrc3

--- packages/leaa-dashboard/src/components/RefreshflatPermissions/RefreshflatPermissions.tsx
+++ packages/leaa-dashboard/src/components/RefreshflatPermissions/RefreshflatPermissions.tsx
@@ -19,7 +19,6 @@ export const RefreshflatPermissions = (props: IProps) => {
       .then((res: IHttpRes<User>) => {
         if (res.data?.data?.flatPermissions && res.data?.data.flatPermissions.length === 0) {
           removeAuth();
-
         }
 
         if (res.data?.data?.flatPermissions && res.data?.data.flatPermissions.length !== 0) {
