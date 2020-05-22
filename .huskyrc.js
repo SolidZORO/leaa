cc5e0dc2aebd9267a39e4aa5c49e645e6701c58a
SolidZORO <solidzoro@live.com>
2020-05-22 16:14:41 +0800

    refactor: test huskyrc4

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,14 @@
 const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
+  // ALL GIT HOOKS DEFINED HERE ARE SUPPORTED
+  // https://git-scm.com/docs/githooks
   hooks: {
+    // https://www.npmjs.com/package/commitizen
+    'prepare-commit-msg': 'git cz --hook || true HUSKY_DEBUG',
+    //this is for linux ==> 'prepare-commit-msg': 'exec < /dev/tty && git cz --hook',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS HUSKY_USE_YARN',
+
     'pre-commit': tasks([
       //check nx before committing!!
       // uncommented for now
@@ -11,8 +18,7 @@ module.exports = {
       'pretty-quick --staged',
       // lint-staged is explained here
       // https://github.com/okonet/lint-staged#configuration.
-      'lint-staged',
+      'lint-staged --debug',
     ]),
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 };
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,15 @@
 module.exports = {
-  'src/**/*.(ts|js)?(x)': ['prettier --write', 'eslint'],
+  //https://github.com/okonet/lint-staged#configuration.
+
+  '*': ['git add'],
+  // 'src/**/*.(ts|js)?(x)': ['prettier --write', 'eslint'],
+  // '*.{json,css,scss,md,js}': ['prettier --write', 'git add'],
+  // '*.(ts)': ['prettier-tslint fix', 'git add'],
+  // '{apps,libs}/**/*.{ts,json,md,scss,html}': [
+  //   //see https://nx.dev/web/api/workspace/npmscripts/affected-lint
+  //   'yarn affected:lint --uncommitted --parallel --fix',
+  //   'yarn format:write --uncommitted',
+  //   'git add',
+  // ],
   // 'packages/**/*.(css|less)': ['prettier --write', 'stylelint"', 'git add'],
 };
