b13f8d349acb00f53d394afeebd5786d8f01b181
SolidZORO <solidzoro@live.com>
2020-05-23 15:12:39 +0800

    fix: fix husky no effect

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,23 +1,17 @@
 const tasks = (arr) => arr.join(' && ');
 
+// ALL GIT HOOKS DEFINED HERE ARE SUPPORTED
+// https://git-scm.com/docs/githooks
+
 module.exports = {
-  // ALL GIT HOOKS DEFINED HERE ARE SUPPORTED
-  // https://git-scm.com/docs/githooks
   hooks: {
-    // https://www.npmjs.com/package/commitizen
-    // 'prepare-commit-msg': 'git cz --hook || true HUSKY_DEBUG',
     //this is for linux ==> 'prepare-commit-msg': 'exec < /dev/tty && git cz --hook',
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+    // https://www.npmjs.com/package/commitizen
+    'prepare-commit-msg': 'git cz --hook || true HUSKY_DEBUG',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS HUSKY_USE_YARN',
     'pre-commit': tasks([
-      //check nx before committing!!
-      // uncommented for now
-      //'nx test',
-      // pretty quick is explained here
-      // https://github.com/azz/pretty-quick
-      'pretty-quick --staged',
-      // lint-staged is explained here
-      // https://github.com/okonet/lint-staged#configuration.
-      'lint-staged --debug',
+      'pretty-quick --staged', // https://github.com/azz/pretty-quick
+      'lint-staged', // https://github.com/okonet/lint-staged#configuration.
     ]),
   },
 };
