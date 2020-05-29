451e4b21a1637d843501ecccf3ebf2a1c53079f9
SolidZORO <solidzoro@live.com>
2020-05-29 15:05:23 +0800

    build: unify config style

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,5 @@
 module.exports = {
   //https://github.com/okonet/lint-staged#configuration.
-
-  // '*': ['git add'],
   'src/**/*.(ts|js)?x': ['prettier --write', 'eslint'],
   // '*.{json,css,scss,md,js}': ['prettier --write', 'git add'],
   // '*.(ts)': ['prettier-tslint fix', 'git add'],
