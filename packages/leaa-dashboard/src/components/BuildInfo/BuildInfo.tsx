5614f84159c74c02039ac75dfb1b27ffad364b30
SolidZORO <solidzoro@live.com>
2020-06-04 01:46:02 +0800

    perf: debugCount 10 to 5

--- packages/leaa-dashboard/src/components/BuildInfo/BuildInfo.tsx
+++ packages/leaa-dashboard/src/components/BuildInfo/BuildInfo.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import cx from 'classnames';
-import { Button, message } from 'antd';
+import { Button } from 'antd';
 
 import { buildConfig } from '@leaa/dashboard/src/configs';
 import { setDebugMode, isDebugMode } from '@leaa/dashboard/src/utils/debug.util';
@@ -15,8 +15,7 @@ export const BuildInfo = (props: IProps) => {
   const [debugCount, setDebugCount] = useState<number>(0);
 
   useEffect(() => {
-    if (debugCount === 7) message.loading('💪 Keep To 10!');
-    if (debugCount === 10) {
+    if (debugCount === 5) {
       console.log('HAAAAAAAAAAAAAAAAAA!');
       setDebugMode(!isDebugMode());
 
@@ -33,7 +32,7 @@ export const BuildInfo = (props: IProps) => {
           <span>MODE:</span>
           <Button type="link" size="small" className={style['switch-debug-module']} onClick={onAdd}>
             <strong>{buildConfig.MODE}</strong>
-            <span className={style['switch-debug-bar']} style={{ width: debugCount * 5 }} />
+            <span className={style['switch-debug-bar']} style={{ width: debugCount * 10 }} />
           </Button>
         </div>
 
