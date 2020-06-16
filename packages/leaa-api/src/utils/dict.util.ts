9e538c0ab5c221cff8a6b034fbc2567a03426bbb
SolidZORO <solidzoro@live.com>
2020-06-17 02:12:43 +0800

    chore: temp remove nodejieba

--- packages/leaa-api/src/utils/dict.util.ts
+++ packages/leaa-api/src/utils/dict.util.ts
@@ -1,21 +1,23 @@
 import fs from 'fs';
-import nodejieba from 'nodejieba';
+// import nodejieba from 'nodejieba';
 
 import { dictConfig } from '@leaa/api/src/configs';
 
 export const cutTags = (text: string, limit = 5): string[] => {
-  if (fs.existsSync(dictConfig.TAGS_DICT_PATH)) {
-    nodejieba.load({
-      // dict: nodejieba.DEFAULT_DICT,
-      // hmmDict: nodejieba.DEFAULT_HMM_DICT,
-      // idfDict: nodejieba.DEFAULT_IDF_DICT,
-      // stopWordDict: nodejieba.DEFAULT_STOP_WORD_DICT,
-      // userDict: '@leaa/api/src/assets/dicts/user.dict.utf8',
-      userDict: dictConfig.TAGS_DICT_PATH,
-    });
-  }
+  return [];
 
-  const jiebaExtractTags: { word: string; weight: number }[] = nodejieba.extract(text, limit);
-
-  return jiebaExtractTags.map((tag) => tag.word);
+  // if (fs.existsSync(dictConfig.TAGS_DICT_PATH)) {
+  //   nodejieba.load({
+  //     // dict: nodejieba.DEFAULT_DICT,
+  //     // hmmDict: nodejieba.DEFAULT_HMM_DICT,
+  //     // idfDict: nodejieba.DEFAULT_IDF_DICT,
+  //     // stopWordDict: nodejieba.DEFAULT_STOP_WORD_DICT,
+  //     // userDict: '@leaa/api/src/assets/dicts/user.dict.utf8',
+  //     userDict: dictConfig.TAGS_DICT_PATH,
+  //   });
+  // }
+  //
+  // const jiebaExtractTags: { word: string; weight: number }[] = nodejieba.extract(text, limit);
+  //
+  // return jiebaExtractTags.map((tag) => tag.word);
 };
