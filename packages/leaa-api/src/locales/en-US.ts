3a0b7b80c5c6bc37f3639702cbfe59880675c9fb
SolidZORO <solidzoro@live.com>
2020-07-06 11:29:48 +0800

    refactor: lang locales to single file

---
+++ packages/leaa-api/src/locales/en-US.ts
@@ -0,0 +1,63 @@
+export default {
+  _error: {
+    test: 'This is a message from the API (en-US / English)',
+    //
+    unauthorized: 'Unauthorized',
+    forbidden: 'Forbidden',
+    badRequest: 'Bad Request',
+    tooManyRequest: 'Too Many Request',
+    //
+    tokenNotFound: 'Token Not Found',
+    tokenError: 'Token Error',
+    tokenNotBefore: 'Token Not Before',
+    tokenExpired: 'Token Expired',
+    tokenVerifyFaild: 'Token Verify Faild',
+    tokenNotPrefix: 'Token Not Prefix',
+    //
+    //
+    notAuth: 'Not Authorized',
+    notFound: 'Not Found',
+    notFoundArgs: 'Not Found Args',
+    notFoundField: 'Not Found Field',
+    notFoundAuth: 'Not Found Auth',
+    notFoundItem: 'Not Found Item',
+    notFoundItems: 'Not Found Items',
+    notFoundUser: 'Not Found User',
+    notFoundId: 'Not Found ID',
+    notFoundIp: 'Not Found IP',
+    notFoundTicket: 'Not Found Ticket',
+    notFoundToken: 'Not Found Token',
+    notFoundInfo: 'Not Found Info',
+    //
+    createItemFailed: 'Create Item Failed',
+    updateItemFailed: 'Update Item Failed',
+    readItemFailed: 'Read Item Failed',
+    deleteItemFailed: 'Delete Item Failed',
+    //
+    illegalUser: 'Illegal User',
+    invalidUser: 'Invalid User',
+    invalidId: 'Invalid ID',
+    invalidIp: 'Invalid IP',
+    invalidHashId: 'Invalid Hash ID',
+    //
+    userInfoNotMatch: 'User Info Not Match',
+    bindingFailed: 'Binding Failed',
+    signupFailed: 'Signup Failed',
+    userHasBeenUpdated: 'User Has Been Updated, Please Login Again',
+    pleaseDontModify: 'PLEASE DONT MODIFY',
+    usernameOrPasswordNotMatch: 'Username Or Password Not Match',
+    verifyCodeNotMatch: 'Verify Code Not Match',
+    //
+    missingFormatArgs: 'Missing Format Args',
+  },
+  _module: {
+    coupon: {
+      unavailable: 'Coupon Unavailable',
+      irredeemable: 'Coupon Irredeemable',
+      alreadyRedeemed: 'Coupon Already redeemed',
+    },
+    promo: {
+      unavailable: 'Promo Unavailable',
+    },
+  },
+};
--- packages/leaa-api/src/locales/en-US/index.ts
+++
@@ -1,4 +0,0 @@
-import _error from './_error.lang';
-import _module from './_module.lang';
-
-export default { _error, _module };
